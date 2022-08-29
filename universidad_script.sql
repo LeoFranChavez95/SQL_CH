adceb7ec92ff8665645aa4bd14c52f2864dd03b3
LeoFranChavez95 <38041848+LeoFranChavez95@users.noreply.github.com>
2022-08-29 11:14:26 -0300

    Update universidad_script.sql

--- universidad_script.sql
+++ universidad_script.sql
@@ -2,6 +2,8 @@
 USE universidad;
 
 
+-- CREACION DE TABLAS
+-- TABLA CATEDRA
 CREATE TABLE catedra(
     codigo_de_catedra INT UNSIGNED NOT NULL AUTO_INCREMENT,
     legajo_docente INT UNSIGNED NOT NULL,
@@ -9,7 +11,7 @@ CREATE TABLE catedra(
     PRIMARY KEY (codigo_de_catedra),
     CONSTRAINT catedra_docente FOREIGN KEY (legajo_docente) REFERENCES docente (legajo_docente)
 )
-
+-- TABLA CURSO
 CREATE TABLE curso (
     codigo_de_curso INT UNSIGNED NOT NULL AUTO_INCREMENT,
     legajo_docente INT UNSIGNED NOT NULL,
@@ -19,7 +21,7 @@ CREATE TABLE curso (
     CONSTRAINT curso_docente FOREIGN KEY (legajo_docente) REFERENCES docente (legajo_docente),
     CONSTRAINT curso_catedra FOREIGN KEY (codigo_de_catedra) REFERENCES catedra (codigo_de_catedra)	
 )
-
+-- TABLA DOCENTE
 CREATE TABLE docente (
     legajo_docente INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(30) NOT NULL,
@@ -27,7 +29,7 @@ CREATE TABLE docente (
     email VARCHAR(30) DEFAULT NULL,
     PRIMARY KEY (legajo_docente)
 )
-
+-- TABLA ESTUDIANTE
 CREATE TABLE estudiante (
     legajo_estudiante INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(30) NOT NULL,
@@ -37,7 +39,7 @@ CREATE TABLE estudiante (
     PRIMARY KEY (legajo_estudiante),
     CONSTRAINT estudiante_carerra FOREIGN KEY (codigo_carrera) REFERENCES carrera (codigo_carrera)
 )
-
+-- TABLA EXAMEN
 CREATE TABLE examen (
     numero_examen INT UNSIGNED NOT NULL AUTO_INCREMENT,
     legajo_estudiante INT UNSIGNED NOT NULL,
@@ -50,7 +52,7 @@ CREATE TABLE examen (
     CONSTRAINT examen_curso FOREIGN KEY (codigo_de_curso) REFERENCES curso (codigo_de_curso)
 )
 
-
+-- TABLA CARRERA
 CREATE TABLE carrera (
     codigo_carrera INT UNSIGNED NOT NULL,
     carrera VARCHAR(30) NOT NULL,
