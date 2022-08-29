f7166c43d83c97dfd1812049f0e8a595505274c7
LeoFranChavez95 <38041848+LeoFranChavez95@users.noreply.github.com>
2022-08-29 11:30:43 -0300

    Update universidad_script.sql

--- universidad_script.sql
+++ universidad_script.sql
@@ -1,28 +1,19 @@
 -- AUTOR : LEONEL FRANKLIN CHAVEZ OLIVARES
 -- CREACION DE BASE
-CREATE SCHEMA universidad;
-USE universidad;
+CREATE SCHEMA universidadPrueba2;
+USE universidadPrueba2;
 
 
 -- CREACION DE TABLAS
--- TABLA CATEDRA
-CREATE TABLE catedra(
-    codigo_de_catedra INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    legajo_docente INT UNSIGNED NOT NULL,
-    catedra VARCHAR(30) NOT NULL,
-    PRIMARY KEY (codigo_de_catedra),
-    CONSTRAINT catedra_docente FOREIGN KEY (legajo_docente) REFERENCES docente (legajo_docente)
-)
--- TABLA CURSO
-CREATE TABLE curso (
-    codigo_de_curso INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    legajo_docente INT UNSIGNED NOT NULL,
-    codigo_de_catedra INT UNSIGNED NOT NULL,
-    fecha_de_inicio DATE DEFAULT NULL,
-    PRIMARY KEY (codigo_de_curso),
-    CONSTRAINT curso_docente FOREIGN KEY (legajo_docente) REFERENCES docente (legajo_docente),
-    CONSTRAINT curso_catedra FOREIGN KEY (codigo_de_catedra) REFERENCES catedra (codigo_de_catedra)	
-)
+
+-- TABLA CARRERA
+CREATE TABLE carrera (
+    codigo_carrera INT UNSIGNED NOT NULL,
+    carrera VARCHAR(30) NOT NULL,
+    plan_de_estudio VARCHAR(10) NOT NULL,
+    PRIMARY KEY (codigo_carrera)
+);
+
 -- TABLA DOCENTE
 CREATE TABLE docente (
     legajo_docente INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -30,7 +21,17 @@ CREATE TABLE docente (
     apellido VARCHAR(30) NOT NULL,
     email VARCHAR(30) DEFAULT NULL,
     PRIMARY KEY (legajo_docente)
-)
+);
+
+-- TABLA CATEDRA
+CREATE TABLE catedra(
+    codigo_de_catedra INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    legajo_docente INT UNSIGNED NOT NULL,
+    catedra VARCHAR(30) NOT NULL,
+    PRIMARY KEY (codigo_de_catedra),
+    CONSTRAINT catedra_docente FOREIGN KEY (legajo_docente) REFERENCES docente (legajo_docente)
+);
+
 -- TABLA ESTUDIANTE
 CREATE TABLE estudiante (
     legajo_estudiante INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -40,7 +41,19 @@ CREATE TABLE estudiante (
     codigo_carrera INT UNSIGNED NOT NULL,
     PRIMARY KEY (legajo_estudiante),
     CONSTRAINT estudiante_carerra FOREIGN KEY (codigo_carrera) REFERENCES carrera (codigo_carrera)
-)
+);
+
+-- TABLA CURSO
+CREATE TABLE curso (
+    codigo_de_curso INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    legajo_docente INT UNSIGNED NOT NULL,
+    codigo_de_catedra INT UNSIGNED NOT NULL,
+    fecha_de_inicio DATE DEFAULT NULL,
+    PRIMARY KEY (codigo_de_curso),
+    CONSTRAINT curso_docente FOREIGN KEY (legajo_docente) REFERENCES docente (legajo_docente),
+    CONSTRAINT curso_catedra FOREIGN KEY (codigo_de_catedra) REFERENCES catedra (codigo_de_catedra)	
+);
+
 -- TABLA EXAMEN
 CREATE TABLE examen (
     numero_examen INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -52,15 +65,7 @@ CREATE TABLE examen (
     PRIMARY KEY (numero_examen),
     CONSTRAINT examen_estudiante FOREIGN KEY (legajo_estudiante) REFERENCES estudiante (legajo_estudiante),
     CONSTRAINT examen_curso FOREIGN KEY (codigo_de_curso) REFERENCES curso (codigo_de_curso)
-)
-
--- TABLA CARRERA
-CREATE TABLE carrera (
-    codigo_carrera INT UNSIGNED NOT NULL,
-    carrera VARCHAR(30) NOT NULL,
-    plan_de_estudio VARCHAR(10) NOT NULL,
-    PRIMARY KEY (codigo_carrera)
-)
+);
 
  -- CREACION DE INDICES
 
